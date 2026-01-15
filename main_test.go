1953562d4e052e573c3c54c34ff9f82e3ce35707
Rohitcodes28 <rohitashgoyal509@gmail.com>
2026-01-15 15:41:50 +0530

    intiial setup ci

---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	expected := 4
+	result := Add(1, 2)
+	if result != expected {
+		t.Errorf("Test Failed: Expected %d, but got %d", expected, result)
+	}
+}
